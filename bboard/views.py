5379747e4bf3ee8e9ff63bfcf2e9e3563f87712d
Valery Gurov <va1ery@ya.ru>
2020-05-11 16:15:34 +0300

    first commit

---
+++ bboard/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from .models import Bb
+
+def index(request):
+    s = 'Список объявлений\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content +'\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
